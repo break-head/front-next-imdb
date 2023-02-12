f4f77bb6b8cade6e86fef300d83db8f1a362d20e
break-head <zkwks0130@gmail.com>
2023-02-13 03:25:36 +0900

    create about page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
       <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
-        <MenuItem title="ABOUT" address="/ABOUT" Icon={BsFillPersonFill} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillPersonFill} />
       </div>
       <div className="flex items-center space-x-5">
         <DarkModeSwitch />
