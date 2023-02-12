c8c94a26e646237aba2d6038b0903574036ed8c9
break-head <zkwks0130@gmail.com>
2023-02-13 03:15:04 +0900

    add dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,19 @@
+import Header from "./components/Header";
 import "./globals.css";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+
+        {/*Navbar*/}
+        {/*SearchBox*/}
+      </body>
     </html>
   );
 }
